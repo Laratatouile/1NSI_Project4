6b5065631afbdf36ce88f644426e54a591420d96
Laratatouile <maxpublic09@gmail.com>
2025-02-13 14:27:28 +0100

    start

---
+++ script.js
@@ -0,0 +1,7 @@
+function bg_color(){
+    if (document.body.style.background == "rgb(75, 75, 75)"){
+        document.body.style.backgroundColor = "rgb(191, 192, 194)";
+    }else{
+        document.body.style.background = "rgb(75, 75, 75)";
+    }
+}
