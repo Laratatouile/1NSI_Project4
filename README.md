af4c12ab9aea27b4a04f41c8c658184952aaa6c2
Laratatouile <maxpublic09@gmail.com>
2025-02-13 14:51:23 +0100

    13/02

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 1NSI_Project4
 project 4 de NSI en 1ere
+
+
+##
+Le fichier index.html contient le travail
--- fonctions_principales.js
+++ fonctions_principales.js
@@ -1,6 +1,6 @@
 function bg_color(){
     if (document.body.style.background == "rgb(75, 75, 75)"){
-        document.body.style.backgroundColor = "rgb(191, 192, 194)";
+        document.body.style.backgroundColor = "rgb(221, 221, 221)";
     }else{
         document.body.style.background = "rgb(75, 75, 75)";
     }
