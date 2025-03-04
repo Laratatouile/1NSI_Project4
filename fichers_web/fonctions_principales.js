7ba4b0bd90eb52c7276f5d5d71f4666d01480592
Laratatouile <maxpublic09@gmail.com>
2025-03-04 22:19:27 +0100

    04/03

--- fichers_web/fonctions_principales.js
+++ fichers_web/fonctions_principales.js
@@ -1,9 +1,38 @@
 function bg_color(){
     var color1 = "rgb(75, 75, 75)";
-    var color2 = "rgb(221, 221, 221)"
+    var color2 = "rgb(221, 221, 221)";
     if (document.body.style.background == color1){
         document.body.style.backgroundColor = color2;
     }else{
         document.body.style.background = color1;
     }
 }
+
+
+const box = document.getElementById("panneau_droite");
+
+box.addEventListener("mouseenter", () => {
+    var color1 = "rgb(75, 75, 75)";
+    var color2 = "rgb(221, 221, 221)";
+    var color3 = "rgb(0, 0, 1)";
+    var color4 = "rgb(0, 0, 0)";
+    if (document.body.style.background == color1){
+        document.body.style.background = color3;
+    }else{
+        document.body.style.background = color4;
+    }
+    document.getElementById("la_page").style.opacity = "0.1";
+});
+
+box.addEventListener("mouseleave", () => {
+    var color1 = "rgb(75, 75, 75)";
+    var color2 = "rgb(221, 221, 221)";
+    var color3 = "rgb(0, 0, 1)";
+    var color4 = "rgb(0, 0, 0)";
+    if (document.body.style.background == color3){
+        document.body.style.background = color1;
+    }else{
+        document.body.style.background = color2;
+    }
+    document.getElementById("la_page").style.opacity = "1";
+});
